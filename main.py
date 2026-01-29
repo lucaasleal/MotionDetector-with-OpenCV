f7088488c12dafdfe488017ad156ac0cb3679de2
lucaasleal <lucasfaleal@gmail.com>
2026-01-29 15:35:35 -0300

    feat:Accumulate diffs added and fine tunning

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-prev_frame = None
+avg_bg = None
  
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
 
 while True:
     ret, frame = cap.read()
@@ -13,17 +15,23 @@ while True:
 
     #Convert colors in grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    if prev_frame is None:
-        prev_frame = gray
+    blur = cv2.GaussianBlur(gray, (21, 21), 0)
+    
+    if avg_bg is None:
+        avg_bg = blur.copy().astype("float")
         continue
+    
+    cv2.accumulateWeighted(gray, avg_bg, 0.02)
+
+    ref_frame = cv2.convertScaleAbs(avg_bg)
 
     #Calcule the diff between previous frame and atual
-    diff = cv2.absdiff(prev_frame, gray)
+    diff = cv2.absdiff(gray, ref_frame)
 
-    blur = cv2.GaussianBlur(diff, (5, 5), 0)
 
-    _, thresh = cv2.threshold(blur, 25, 255, cv2.THRESH_BINARY)
+    _, thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
+
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
 
     thresh = cv2.dilate(thresh, None, iterations=2)
 
@@ -33,15 +41,13 @@ while True:
     )
 
     for contour in contours:
-        if cv2.contourArea(contour) < 300:
+        if cv2.contourArea(contour) < 4000:
             continue
 
         x, y, w, h = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
 
     cv2.imshow("Frame", frame)
-    cv2.imshow("Difference", diff)
-    cv2.imshow("Threshold", thresh)
 
     prev_frame = gray
     
